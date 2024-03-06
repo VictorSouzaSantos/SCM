372082915c7a0adaef1d1278369d772de415411a
VictorSouzaSantos <160680811+VictorSouzaSantos@users.noreply.github.com>
2024-03-06 19:05:45 -0300

    Add files via upload

---
+++ Program.java
@@ -0,0 +1,29 @@
+package aplication;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import utilidades.CurrencyConverter;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		/*System.out.print("Qual o valor do dollar? ");
+		double precoDollar = sc.nextDouble();
+		System.out.print("Quantos dollares será comprado? ");
+		double quantia = sc.nextDouble();
+		double result = CurrencyConverter.dollarParaReal(quantia, precoDollar);
+		System.out.printf("Valor a ser pago em reais = %.2f%n", result);*/
+		
+		
+		
+		
+		sc.close();
+
+	}
+
+}
